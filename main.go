56862f8eb3f8347ec00659abffe1512506d3525e
wscnd <72019998+wscnd@users.noreply.github.com>
2024-04-05 17:52:13 -0300

    make pod respect pod resource restrictions

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"runtime"
 	"syscall"
 )
 
 var build = "develop"
 
 func main() {
-	log.Println("starting service", build)
+	log.Printf("starting service build[%s] CPU[%v]", build, runtime.GOMAXPROCS(0))
+
 	defer log.Println("service ended")
 
 	shutdown := make(chan os.Signal, 1)
