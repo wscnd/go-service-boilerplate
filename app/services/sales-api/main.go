e732df1ea367b74fbbd30acbdc0a0e2f552c35b4
wscnd <72019998+wscnd@users.noreply.github.com>
2024-04-07 10:40:38 -0300

    use custom logger in sales-api service

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,23 +1,46 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
+
+	"github.com/wscnd/go-service-boilerplate/foundation/logger"
 )
 
 var build = "develop"
 
 func main() {
-	log.Printf("starting service build[%s] CPU[%v]", build, runtime.GOMAXPROCS(0))
+	var log *logger.Logger
 
-	defer log.Println("service ended")
+	log = logger.NewWithEvents(
+		os.Stdout,
+		logger.LevelInfo,
+		"SALES-API",
+		func(ctx context.Context) string {
+			return "id-123123-123123"
+		},
+		logger.Events{
+			Error: func(ctx context.Context, r logger.Record) {
+				log.Info(ctx, "*** alert ***")
+			},
+		},
+	)
+
+	// _______________
+
+	ctx := context.Background()
+	log.Info(ctx, fmt.Sprintf("starting service build[%s] CPU[%v]", build, runtime.GOMAXPROCS(0)))
+
+	defer log.Info(ctx, "service ended")
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 
 	<-shutdown
-	log.Println("stopping service")
+
+	log.Info(ctx, "stopping service")
 }
