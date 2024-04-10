001e73375536528df4867d193d22745f79aeba6b
wscnd <72019998+wscnd@users.noreply.github.com>
2024-04-09 23:26:32 -0300

    add middleware & handle route/app layers middlewares

--- libs/web/web.go
+++ libs/web/web.go
@@ -12,21 +12,26 @@ type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) e
 type App struct {
 	*http.ServeMux
 	shutdown chan os.Signal
+	mws      []Middleware
 }
 
-func (a *App) Handle(pattern string, handler Handler) {
-	h := func(w http.ResponseWriter, r *http.Request) {
-		if err := handler(r.Context(), w, r); err != nil {
-			a.shutdown <- syscall.SIGTERM
-		}
-	}
-	a.ServeMux.HandleFunc(pattern, h)
-}
-
-func NewApp(shutdown chan os.Signal) *App {
+func NewApp(shutdown chan os.Signal, mws ...Middleware) *App {
 	mux := http.NewServeMux()
 	return &App{
 		ServeMux: mux,
 		shutdown: shutdown,
+		mws:      mws,
 	}
+}
+
+func (app *App) Handle(pattern string, handler Handler, routemws ...Middleware) {
+	handler = wrapMiddlewares(routemws, handler)
+	handler = wrapMiddlewares(app.mws, handler)
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		if err := handler(r.Context(), w, r); err != nil {
+			app.shutdown <- syscall.SIGTERM
+		}
+	}
+	app.ServeMux.HandleFunc(pattern, h)
 }
