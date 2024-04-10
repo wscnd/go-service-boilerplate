d3c7e6fe50fc471cbda7e1d23e95deac0d2d5bee
wscnd <72019998+wscnd@users.noreply.github.com>
2024-04-09 23:25:03 -0300

    add json wrapper to response

--- apps/server/sales/routes/homeapi/home.go
+++ apps/server/sales/routes/homeapi/home.go
@@ -2,18 +2,16 @@ package homeapi
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
+	"github.com/wscnd/go-service-boilerplate/libs/web"
 )
 
 func homeHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	status := struct {
 		Status string
 	}{
-		Status: "ok",
+		Status: "ok embedded",
 	}
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(&status)
-	return nil
+	return web.RespondJSON(ctx, w, status, http.StatusOK)
 }
 
