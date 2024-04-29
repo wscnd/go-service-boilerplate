582ec8eb0e232e84b7d7afde06ec5f48d864afc4
wscnd <72019998+wscnd@users.noreply.github.com>
2024-04-29 16:42:07 -0300

    add auth api

--- apis/auth/auth.go
+++ apis/auth/auth.go
@@ -1,7 +1,14 @@
 package auth
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/open-policy-agent/opa/rego"
+	"github.com/wscnd/go-service-boilerplate/libs/logger"
 )
 
 // TokenClaims represents the authorization claims transmitted via a JWT.
@@ -9,3 +16,40 @@ type TokenClaims struct {
 	jwt.RegisteredClaims
 	Roles []string `json:"roles"`
 }
+
+// KeyLookup declares a method set of behavior for looking up
+// private and public keys for JWT use. The return could be a
+// PEM encoded string or a JWS based key.
+type KeyLookup interface {
+	PrivateKey(keyID string) (key string, err error)
+	PublicKey(keyID string) (key string, err error)
+}
+
+// Config represents information required to initialize auth.
+type Config struct {
+	Log       *logger.Logger
+	KeyLookup KeyLookup
+	Issuer    string
+}
+
+// Auth is used to authenticate clients. It can generate a token for a
+// set of user claims and recreate the claims by parsing the token.
+type Auth struct {
+	keyLookup KeyLookup
+	method    jwt.SigningMethod
+	parser    *jwt.Parser
+	issuer    string
+}
+
+// New creates an Auth to support authentication/authorization.
+func New(cfg Config) (*Auth, error) {
+	a := Auth{
+		keyLookup: cfg.KeyLookup,
+		method:    jwt.GetSigningMethod(jwt.SigningMethodRS256.Name),
+		parser:    jwt.NewParser(jwt.WithValidMethods([]string{jwt.SigningMethodRS256.Name})),
+		issuer:    cfg.Issuer,
+	}
+
+	return &a, nil
+}
+
