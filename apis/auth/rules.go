687e8656b02fa6cbbfcc4eef77958d525892b7e8
wscnd <72019998+wscnd@users.noreply.github.com>
2024-04-29 18:03:40 -0300

    create auth rules & rego for authZ

--- apis/auth/rules.go
+++ apis/auth/rules.go
@@ -4,16 +4,23 @@ import _ "embed"
 
 // Package name of our rego code.
 const (
-	OpaPackage string = "serviceAuth.rego"
+	opaPackage string = "serviceAuth.rego"
 )
 
 // These the current set of rules we have for auth.
 const (
-	RuleAuthenticate = "auth"
+	RuleAuthenticate   = "auth"
+	RuleAny            = "rule_any"
+	RuleAdminOnly      = "rule_admin_only"
+	RuleUserOnly       = "rule_user_only"
+	RuleAdminOrSubject = "rule_admin_or_subject"
 )
 
 // Core OPA policies.
 var (
 	//go:embed rego/authentication.rego
-	OpaAuthentication string
+	opaAuthentication string
+
+	//go:embed rego/authorization.rego
+	opaAuthorization string
 )
